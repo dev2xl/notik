58528b99bbd25a307c7bb5325e537ceb5a4e5835
Alberto Sosa <146108762+dev2xl@users.noreply.github.com>
2023-11-26 21:16:41 -0500

    Added PINECONE_INDEX and PINECONE_ENVIRONMENT environment variables

--- src/lib/db/pinecone.ts
+++ src/lib/db/pinecone.ts
@@ -1,11 +1,15 @@
 import { Pinecone } from "@pinecone-database/pinecone";
 
 const apiKey = process.env.PINECONE_API_KEY;
+const index = process.env.PINECONE_INDEX;
+const environment = process.env.PINECONE_ENVIRONMENT;
 
-if (!apiKey) {
-  throw Error("PINECONE_API_KEY is not set");
+if (!apiKey || !index || !environment) {
+  throw Error(
+    "PINECONE_API_KEY or PINECONE_INDEX or PINECONE_ENVIRONMENT is not set",
+  );
 }
 
-const pinecone = new Pinecone({ environment: "gcp-starter", apiKey });
+const pinecone = new Pinecone({ environment, apiKey });
 
-export const notesIndex = pinecone.Index("nextjs-notes-ai");
+export const notesIndex = pinecone.Index(index);
